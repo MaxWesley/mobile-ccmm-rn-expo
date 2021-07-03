562c2442b1f8f893f9dfa22b8d36c5c9308f7993
MaxWesley <mx_wesley@hotmail.com>
2021-07-03 02:39:33 -0300

    feat: prepare for receive cards

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,54 @@
 import React from 'react';
 
-import { View } from "react-native";
+import { View, ScrollView, Text } from "react-native";
 
 import { Header } from '../../components/Header';
 
 import { styles } from './styles';
 
-export function Home() {
-    const { container } = styles;
+import { Card } from '../../components/Card';
+import { Footer } from '../../components/Footer';
 
-    return(
+export function Home() {
+    const { container, containerCards } = styles;
+
+    return (
         <View style={container}>
             <Header />
+            <ScrollView style={containerCards}>
+                <Card
+                    name="Max Wesley"
+                    description="Avenida Querqueiro, n7 casa 3"
+                />
+                <Card
+                    name="Max Wesley"
+                    description="Avenida Querqueiro, n7 casa 3"
+                />
+                <Card
+                    name="Max Wesley"
+                    description="Avenida Querqueiro, n7 casa 3"
+                />
+                <Card
+                    name="Max Wesley"
+                    description="Avenida Querqueiro, n7 casa 3"
+                />
+                <Card
+                    name="Max Wesley"
+                    description="Avenida Querqueiro, n7 casa 3"
+                />
+                <Card
+                    name="Max Wesley"
+                    description="Avenida Querqueiro, n7 casa 3"
+                />
+                <Card
+                    name="Max Wesley"
+                    description="Avenida Querqueiro, n7 casa 3"
+                />
+                <Card
+                    name="Max Wesley"
+                    description="Avenida Querqueiro, n7 casa 3"
+                />
+            </ScrollView>
         </View>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,6 +2,11 @@ import { StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        minHeight: 750,
+        maxHeight: 750
+    },
+    containerCards: {
+        paddingHorizontal: 24,
     }
 });
