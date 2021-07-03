aeff157419bf4631fbcdf7a82d297455db1a2234
MaxWesley <mx_wesley@hotmail.com>
2021-07-03 01:58:27 -0300

    feat: add container label texts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,13 +7,18 @@ import { styles } from './styles';
 import { SearchBar } from '../SearchBar';
 
 export function Header() {
-    const { container, textLogo } = styles;
+    const { container, textLogo, labelBusca, textLabel1, textLabel2 } = styles;
     
     return (
         <View style={container}>
             <Text style={textLogo}>CCMM</Text>
-
             <SearchBar />
+            <View style={labelBusca}>
+                <Text>
+                    <Text style={textLabel1}>Resultado da busca: </Text>
+                    <Text style={textLabel2}>Todos moradores</Text>
+                </Text>
+            </View>
         </View>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
     container: {
@@ -14,5 +15,21 @@ export const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 24,
         color: "#83868A",
-    }
+    },
+    labelBusca: {
+        width: '100%',
+        marginTop: 27,
+        borderBottomWidth: 1,
+        borderBottomColor: theme.colors.borderBottom,
+        paddingBottom: 8,
+        marginBottom: 20
+    },
+    textLabel1: {
+        color: theme.colors.labelLight,
+        fontWeight: '600'
+    },
+    textLabel2: {
+        color: theme.colors.labelBold,
+        fontWeight: 'bold'
+    },
 });
