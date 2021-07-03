136e95687937ef1a1c888fb8ac80ad79e8350166
MaxWesley <mx_wesley@hotmail.com>
2021-07-03 01:06:22 -0300

    feat: create Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: 56,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textLogo: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        color: "#83868A"
+    }
+});
